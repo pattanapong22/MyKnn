2149e585619ea8bd752ba71675fd089fbdec3141
pattanapong22 <tenzaza166@gmail.com>
2024-11-19 14:57:04 +0700

    .

--- home.py
+++ home.py
@@ -7,6 +7,6 @@ st.header("Website Deverloper Profesional Python")
 st.image('./img/1.jpg')
 st.subheader("Pattanapong Sukhamsaen")
 
-dt=pd.read_csv('./data/iris-3.csv')
+dt=pd.read_csv('./data/iris.csv')
 st.header()
 st.write(dt.head(10))
